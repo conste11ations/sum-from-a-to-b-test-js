b82ca9c2584b7bf37605b7eba0159a2ce5b53559
Rachel Kat <rachelkat@gmail.com>
2020-04-04 00:44:32 +0000

    add index.js for recursive sum

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  if ((fromN === toN)) {
+    return fromN;
+  } else if ((toN - fromN) === 1) {
+    return fromN + toN;
+  }
+  return fromN + toN + sum(fromN + 1, toN - 1);
 }
+
+console.log(sum(3, 7));
+
 
 module.exports = sum;
